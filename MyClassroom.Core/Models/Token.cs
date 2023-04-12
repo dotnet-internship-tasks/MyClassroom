1c87567ba8ecb15a93e6c5d2cb58ce0ef7792a8e
CristianB13 <cristian36200119@gmail.com>
2023-04-12 07:17:48 +0300

    add models for auth

---
+++ MyClassroom.Core/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassroom.Core.Models
+{
+    public class Token
+    {
+        public Guid Id { get; set; }
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpireDate { get; set; }
+        public DateTime RefreshTokenExpireDate { get; set; }
+    }
+}
