54071d5bdcefd836c11ebc32a2867a8e4de3d4de
CristianB13 <cristian36200119@gmail.com>
2023-04-12 08:05:21 +0300

    add the necessary services in Program.cs

--- MyClassroom/Program.cs
+++ MyClassroom/Program.cs
@@ -1,7 +1,46 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using MyClassroom.Application.Services;
+using MyClassroom.Infrastructure.Data;
+using MyClassroom.MVC.Auth;
+using MyClassroom.MVC.Github;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.Configure<GithubAppOptions>(builder.Configuration.GetSection(GithubAppOptions.Name));
+builder.Services.AddScoped<IClaimsTransformation, RoleClaimsTransformation>();
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie", options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.Events.OnSigningIn = AuthEventsFunctions.OnSigningIn;
+    })
+    .AddGithub(options =>
+    {
+        options.SignInScheme = "cookie";
+
+        GithubAppOptions githubAppOptions = new();
+        builder.Configuration.GetSection(GithubAppOptions.Name).Bind(githubAppOptions);
+        options.ClientId = githubAppOptions.ClientId;
+        options.ClientSecret = githubAppOptions.ClientSecret;
+
+        options.ClaimActions.MapJsonKey("github_id", "id");
+        options.ClaimActions.MapJsonKey("github_login", "login");
+        options.ClaimActions.MapCustomJson("session_id", (e) => Guid.NewGuid().ToString());
+
+        options.Events.OnCreatingTicket = AuthEventsFunctions.OnCreatingTicket;
+    });
+
+builder.Services.AddDbContext<MyClassroomContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("MyClassroom.MVC"));
+});
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
@@ -17,7 +56,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
