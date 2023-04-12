0e0258c78a50e44d96e356536f44efbafd567eea
CristianB13 <cristian36200119@gmail.com>
2023-04-12 07:33:31 +0300

    add token and user services

---
+++ MyClassroom.Application/Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using MyClassroom.Core.Models;
+
+namespace MyClassroom.Application.Services
+{
+    public interface ITokenService
+    {
+        Task<Token> AddToken(Token token);
+        Task<Token?> GetTokenById(Guid id);
+    }
+}
