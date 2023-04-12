5ea25281242474ae46874579f7e9f919e745e942
CristianB13 <cristian36200119@gmail.com>
2023-04-12 07:36:02 +0300

    add github defaults for setting oauth

---
+++ MyClassroom/Github/GithubAppOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace MyClassroom.MVC.Github
+{
+    public class GithubAppOptions
+    {
+        public const string Name = "GithubApp";
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+    }
+}
---
+++ MyClassroom/Github/GithubOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.Extensions.Options;
+using MyClassroom.Application.Services;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace MyClassroom.MVC.Github
+{
+    public class GithubOptions : OAuthOptions
+    {
+        public GithubOptions()
+        {
+            AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+            TokenEndpoint = "https://github.com/login/oauth/access_token";
+            CallbackPath = new PathString("/signin-github");
+            UserInformationEndpoint = "https://api.github.com/user";
+        }
+    }
+}
