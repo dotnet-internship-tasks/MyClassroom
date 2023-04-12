6a1356172a8a2ae677e9487907e032fb610220b7
CristianB13 <cristian36200119@gmail.com>
2023-04-12 07:20:49 +0300

    add MyClassroomContext in Infrastructure layer

---
+++ MyClassroom.Infrastructure/Data/MyClassroomContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyClassroom.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassroom.Infrastructure.Data
+{
+    public class MyClassroomContext : DbContext
+    {
+        public MyClassroomContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+    }
+}
